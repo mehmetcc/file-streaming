1df501428bdeebdafa307d8c41a017809a5da8bc
mehmetcc <mehmetccm@gmail.com>
2024-12-09 20:18:57 +0300

    Update some operational functions

--- src/main/scala/commons/FileOps.scala
+++ src/main/scala/commons/FileOps.scala
@@ -3,15 +3,28 @@ package commons
 import zio.{Task, ZIO}
 
 import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Path, Paths, StandardOpenOption}
+import java.nio.file.{Files, Path, StandardOpenOption}
 
 object FileOps {
-  def write(path: String, content: Array[Int]): Task[Path] = ZIO.attempt {
+  def write(path: String, content: Array[Int]): Task[Path] = PathOps.of(path).flatMap(write(_, content))
+
+  def write(path: Path, content: Array[Int]): Task[Path] = ZIO.attempt {
     Files.write(
-      Paths.get(path),
+      path,
       content.mkString("\n").getBytes(StandardCharsets.UTF_8),
       StandardOpenOption.CREATE,
       StandardOpenOption.TRUNCATE_EXISTING
+    )
+  }
+
+  def append(path: String, content: Array[Int]): Task[Path] = PathOps.of(path).flatMap(append(_, content))
+
+  def append(path: Path, content: Array[Int]): Task[Path] = ZIO.attempt {
+    Files.write(
+      path,
+      content.mkString("\n").getBytes(StandardCharsets.UTF_8),
+      StandardOpenOption.CREATE,
+      StandardOpenOption.APPEND
     )
   }
 }
--- src/main/scala/commons/FolderOps.scala
+++ src/main/scala/commons/FolderOps.scala
@@ -6,14 +6,14 @@ import java.nio.file.{Files, Path}
 import java.util.Comparator
 
 object FolderOps {
-  def create(path: String): Task[Option[Path]] = PathOps.of(path).flatMap(path => create(path))
+  def create(path: String): Task[Option[Path]] = PathOps.of(path).flatMap(create)
 
   def create(path: Path): Task[Option[Path]] = ZIO.succeed {
     if (!Files.exists(path)) Some(Files.createDirectories(path))
     else None
   }
 
-  def delete(path: String): Task[Unit] = PathOps.of(path).flatMap(path => delete(path))
+  def delete(path: String): Task[Unit] = PathOps.of(path).flatMap(delete)
 
   def delete(path: Path): Task[Unit] = ZIO.attempt {
     Files
