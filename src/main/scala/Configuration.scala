a689ac902d4e300c702dcadbef31d2cf8f43e686
mehmetcc <mehmetccm@gmail.com>
2024-12-09 11:50:00 +0300

    Fix unlabeled values inside Configuration.scala

--- src/main/scala/Configuration.scala
+++ src/main/scala/Configuration.scala
@@ -2,7 +2,7 @@ import zio._
 import zio.config.magnolia.deriveConfig
 import zio.config.typesafe.TypesafeConfigProvider
 
-case class Configuration(chunkSize: Int, intermediaryDirectory: String)
+case class Configuration(chunkSize: Int, intermediaryDirectory: String, inputPath: String, outputPath: String)
 
 object Configuration {
   private val config: Config[Configuration] = deriveConfig[Configuration].nested("application")
